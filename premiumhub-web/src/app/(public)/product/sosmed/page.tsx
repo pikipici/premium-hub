009b8c7462cd4b35542f7a8eaa029e8c0b057c3d
pikipici <fikriramaaulia@gmail.com>
2026-05-01 21:13:09 +0700

    fix: syntax error in page.tsx

--- premiumhub-web/src/app/(public)/product/sosmed/page.tsx
+++ premiumhub-web/src/app/(public)/product/sosmed/page.tsx
@@ -346,7 +346,7 @@ export default function ProductSosmedLandingPage() {
                   </div>
                 </article>
               )
-            })}}
+            })}
           </div>
           )}
 
